3776e3caed7ce24b4f98dd07dcc36b90f6623ddb
andrejusnec <73877307+andrejusnec@users.noreply.github.com>
2020-12-29 13:59:06 +0200

    Menesio pavadinimai

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
 const account = [
-    { month: 10, income: 150 },
-    { month: 1, income: 200, expense: 50 },
-    { month: 3, income: 450, expense: 200 },
-    { month: 7, expense: 50 },
-    { month: 2, income: 50 },
-    { month: 5, income: 150, expense: 50 },
-    { month: 9, income: 700, expense: 400 },
-    { month: 4, income: 10, expense: 300 },
-    { month: 6, expense: 50 },
-    { month: 11, income: 150, expense: 30 },
-    { month: 8, income: 450, expense: 120 },
-    { month: 12, expense: 200 }
+    {name: 'Sausis', month: 10, income: 150 },
+    {name: 'Vasaris', month: 1, income: 200, expense: 50 },
+    {name: 'Kovas', month: 3, income: 450, expense: 200 },
+    {name: 'Balandis', month: 7, expense: 50 },
+    {name: 'Geguze', month: 2, income: 50 },
+    {name: 'Birzelis', month: 5, income: 150, expense: 50 },
+    {name: 'Liepa', month: 9, income: 700, expense: 400 },
+    {name: 'Rugpjutis', month: 4, income: 10, expense: 300 },
+    {name: 'Rugsejis', month: 6, expense: 50 },
+    {name: 'Spalis', month: 11, income: 150, expense: 30 },
+    {name: 'Lapkritis', month: 8, income: 450, expense: 120 },
+    {name: 'Gruodis', month: 12, expense: 200 }
 ];
